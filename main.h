911e04f58843d8671a73c92de67e77ebf747b35f
basele <s.basele@gmail.com>
2023-04-25 05:06:05 -0700

    Betty Compliant main.h

--- main.h
+++ main.h
@@ -1,8 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 #include <stdlib.h>
-int _printf(const char *format, ...);
+
+/**
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} 
+print_t;
 
 #endif
